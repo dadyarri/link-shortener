1a3912ad8a5c0f55ef6a91afad18c30a6774168d
Daniil <dadyarri@gmail.com>
2022-08-20 21:11:19 +0300

    design form to shorten link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,28 @@
 import type {NextPage} from 'next'
+import {Button, Container, Flex, FormControl, FormLabel, Heading, Input, InputGroup, InputLeftAddon} from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   return (
-    <></>
+    <Container mt={3}>
+      <Flex alignItems={"center"} direction={"column"}>
+        <Heading as={"h1"}>Сокращатель ссылок</Heading>
+
+        <FormControl as={"fieldset"} mt={4}>
+          <FormLabel as={"legend"}>Полный URL</FormLabel>
+          <Input type={"url"} placeholder={"URL"}/>
+        </FormControl>
+
+        <FormControl as={"fieldset"} mt={4}>
+          <FormLabel as={"legend"}>Слаг</FormLabel>
+          <InputGroup>
+            <InputLeftAddon>https://link.dadyarri.ru/</InputLeftAddon>
+            <Input/>
+          </InputGroup>
+
+          <Button type={"submit"} mt={4}>Сократить!</Button>
+        </FormControl>
+      </Flex>
+    </Container>
   )
 }
 
