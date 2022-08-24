39b789da595abc4edf94bc99e2b7e9c63ea2961a
Daniil <dadyarri@gmail.com>
2022-08-24 15:21:11 +0300

    add api endpoint to get long url from storage

---
+++ pages/api/getUrl/[slug].ts
@@ -0,0 +1,28 @@
+import { createClient } from "redis";
+import { get_redis_url } from "../../../libs/utils";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const client = createClient({
+    url: get_redis_url()
+  });
+
+  let { slug } = req.query;
+  slug = slug as string;
+
+  client.on("error", err => {
+    console.error("Redis client error: ", err);
+  });
+
+  await client.connect();
+
+  const longUrl = await client.hGet(slug, "url");
+  if (longUrl) {
+    return res.status(200).json({ url: longUrl });
+  } else {
+    return res.status(404).json({ error: "Not found" });
+  }
+}
