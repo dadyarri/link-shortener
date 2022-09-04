88811a33ec20592c93a0c0b51b3bd6e3afa30e00
Daniil <dadyarri@gmail.com>
2022-09-04 23:01:57 +0300

    try to add header to allow api request from schedule site

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import type {AppProps} from "next/app";
+import {ChakraProvider} from "@chakra-ui/react";
 import theme from "../libs/theme";
+
 
 function Website({ Component, pageProps }: AppProps) {
   return (
@@ -8,6 +9,16 @@ function Website({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
     </ChakraProvider>
   );
+}
+
+export async function getServerSideProps(context: { res: { setHeader: (arg0: string, arg1: string) => void; }; }) {
+
+  // set HTTP header
+  context.res.setHeader('Access-Control-Allow-Origin', 'schedule.dadyarri.ru')
+
+  return {
+    props: {}, // will be passed to the page component as props
+  }
 }
 
 export default Website;
